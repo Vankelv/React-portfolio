d143fc1412aa1956234e7671cfe0a18d623a5006
Kelvin Samuel Klutse <88723961+Vankelv@users.noreply.github.com>
2023-07-05 12:55:58 +0000

    Update navbar.js

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,37 +1,67 @@
 import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from 'react-scroll';
-import { Row, Col } from 'react-bootstrap';
-import LogoImage from '../images/Logo.png';
-import "../styles/main.css"
+import { Link } from "react-scroll";
+import { Row, Col } from "react-bootstrap";
+import LogoImage from "../images/Logo.png";
+import "../styles/main.css";
 
 function Navbar() {
-    const navRef = useRef()
+  const navRef = useRef();
 
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav")
-    }
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-    return (
-        <header style={{padding: '10px'}}>
-            <Col xs={12} md={6} className="text-center">
-          <img src={LogoImage} alt="Van logo" /><span style={{marginLeft: '20px', fontFamily:'Maven pro',fontWeight:'700', color:'#cccaca', fontSize: '16px'}}>Frontend | Branding | IT support</span>
-        </Col>
-            <nav ref={navRef}>
-                <Link to="hero" smooth={true} duration={500} onClick={() => showNavbar()}><a href="#home">Home</a></Link>
-                <Link to="portfolio" smooth={true} duration={400} onClick={() => showNavbar()}><a href="#portfolio">My works</a></Link>
-                <Link to="about" smooth={true} duration={500} onClick={() => showNavbar()}><a href="#about">About</a></Link>
-                <div className="close-btn" onClick={() => showNavbar()}>
-    <FaTimes />
-  </div>
-                {/* <button className="nav-contact" ><a href="/#">Contact me</a></button> */}
-               
-            </nav>
-            <button className="nav-btn" onClick={showNavbar}>
-                <FaBars />
-            </button>
-        </header>
-    );
+  return (
+    <header style={{ padding: "10px" }}>
+      <Col xs={12} md={6} className="text-center">
+        <span
+          style={{
+            marginLeft: "20px",
+            fontFamily: "Maven pro",
+            fontWeight: "700",
+            color: "#cccaca",
+            fontSize: "16px",
+          }}
+        >
+          Frontend | Branding | IT support
+        </span>
+      </Col>
+      <nav ref={navRef}>
+        <Link
+          to="hero"
+          smooth={true}
+          duration={500}
+          onClick={() => showNavbar()}
+        >
+          <a href="#home">Home</a>
+        </Link>
+        <Link
+          to="portfolio"
+          smooth={true}
+          duration={400}
+          onClick={() => showNavbar()}
+        >
+          <a href="#portfolio">My works</a>
+        </Link>
+        <Link
+          to="about"
+          smooth={true}
+          duration={500}
+          onClick={() => showNavbar()}
+        >
+          <a href="#about">About</a>
+        </Link>
+        <div className="close-btn" onClick={() => showNavbar()}>
+          <FaTimes />
+        </div>
+        {/* <button className="nav-contact" ><a href="/#">Contact me</a></button> */}
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
 
 export default Navbar;
